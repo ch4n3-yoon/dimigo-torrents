c77f69689601379b6d32bfe09e688b74facbf83d
ch4n3-yoon <chaneyoon@gmail.com>
2018-10-17 22:37:58 +0900

    필요없는 변수 삭제

--- run.py
+++ run.py
@@ -22,21 +22,6 @@ def getContents(url):
     response = subprocess.getoutput("curl \"%s\"" % (url))
     return response
 
-month = {
-    'Jan': 1,
-    'Feb': 2,
-    'Mar': 3,
-    'Apr': 4,
-    'May': 5,
-    'Jun': 6,
-    'Jul': 7,
-    'Aug': 8,
-    'Sep': 9,
-    'Oct': 10,
-    'Nov': 11,
-    'Dec': 12,
-}
-
 def getTorrents(ip):
     url = 'https://iknowwhatyoudownload.com/en/peer/?ip=%s' % (ip)
     soup = BeautifulSoup(getContents(url), 'lxml')
